f41d3ea301fe0c05cb83c1ac68555da197d0d591
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-08-26 22:01:45 -0300

    att

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,1 +1,24 @@
-window.alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML=`Agora são ${hora} horas.`
+    if(hora >=0 &&  hora < 12){
+        // Bom dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background='#f8d7a9'
+    } else if(hora >= 12 && hora <= 18 ){
+        // Boa tarde!
+        img.src = 'fototarde.png'
+        document.body.style.background='#7e4c56'
+
+    } else {
+        // Boa noite!
+        img.src= 'fotonoite.png'
+        document.body.style.background ='#3679ac'
+    }
+
+
+}
+
