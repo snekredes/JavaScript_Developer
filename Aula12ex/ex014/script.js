6dfc20dd9e7443b03d568a52f6482ea0e4f4b588
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-08-26 22:11:02 -0300

    att

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -4,7 +4,7 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML=`Agora são ${hora} horas.`
-    if(hora >=0 &&  hora < 12){
+    if (hora >= 0 &&  hora < 12){
         // Bom dia!
         img.src = 'fotomanha.png'
         document.body.style.background='#f8d7a9'
