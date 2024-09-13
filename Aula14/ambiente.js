e70c925018cc42340be5af16ea5043f06deecd5f
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-13 20:51:34 -0300

    att

---
+++ Aula14/ambiente.js
@@ -0,0 +1,19 @@
+
+console.log('Vai começar ....')
+for(c=1;c<=6;c++){
+    console.log(c)
+
+}
+console.log('FIM!')
+
+
+
+
+/*
+var c = 1 
+for( c=1;c<=25;c++){
+    console.log(`Passo${c}`)
+
+}
+
+*/
