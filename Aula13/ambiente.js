a04ab1e3d4763a2d9fb99c54b7f76b5911e9a1c4
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-13 20:08:34 -0300

    att

---
+++ Aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+while(c<=8){
+    console.log('Olá tudo bem !')
+    c++
+}
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
