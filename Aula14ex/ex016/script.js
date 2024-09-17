8ce81d6171229131fe39a34543ead98c425f8aee
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-17 20:13:20 -0300

    att

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,1 +1,36 @@
-window.alert('Olá')
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo =document.getElementById('txtp')
+    let res =document.getElementById('res')
+
+    if(ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.length == 0){
+        res.innerHTML = `Impossível contar!`
+        window.alert('[Erro] Faltam dados por favor preencha corretamente!')
+    } else{
+        res.innerHTML='Contando <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p<=0){
+            window.alert('Passo inválido! Considerando o Passo 1')
+            p=1
+        }
+        if(i < f){
+            //Contagem Crescente
+        for(let c=i; c<=f; c+=p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+      
+    } else{
+        //Contagem  Regressiva
+        for(let c=i; c>=f; c-=p ){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        
+    }
+      res.innerHTML += `\u{1F6A9}`
+  }
+
+
+}
