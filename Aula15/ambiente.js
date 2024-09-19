bd8a4276b4f838e7033c313f057dafcc2fe98d49
Renato Alves <67865404+snekredes@users.noreply.github.com>
2024-09-19 18:29:40 -0300

    att

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -4,3 +4,12 @@ num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições!`)
 console.log(`Nosso vetor seria ${num[0]}`)
+let pos= num.indexOf(10)
+if( pos == -1){
+    console.log(`O valor não foi encontrado!`)
+
+    }else{ 
+        console.log(`O valor está na posição ${pos}`)
+   
+}
+
